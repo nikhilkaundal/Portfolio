7b5d1bd6ce7f0d95e3facb70c639eeebabe3700f
nikhil1234567890123 <nikhilkaundal1247@gmail.com>
2026-06-25 15:34:06 +0530

    style: fix chrome color rendering using composite atop and invisible SVG container

--- portfolio_v3/src/components/ui/Cursor.tsx
+++ portfolio_v3/src/components/ui/Cursor.tsx
@@ -25,41 +25,24 @@ const Cursor: React.FC = () => {
 
   return (
     <>
-      {/* Hidden SVG for the gooey liquid mercury 3D specular chrome filter */}
-      <svg xmlns="http://www.w3.org/2000/svg" version="1.1" style={{ display: "none" }}>
+      {/* Hidden SVG container - styled to remain in layout so filters compile, but invisible to user */}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+        style={{ position: "absolute", width: 0, height: 0, pointerEvents: "none", opacity: 0 }}
+      >
         <defs>
-          <filter id="gooey-cursor">
-            {/* 1. Gooey blur and alpha threshold to merge shapes into organic blobs */}
-            <feGaussianBlur in="SourceGraphic" stdDeviation="6" result="blur" />
+          <filter id="gooey-cursor" x="-50%" y="-50%" width="200%" height="200%">
+            {/* 1. Blur and threshold to create the organic liquid merging mask */}
+            <feGaussianBlur in="SourceGraphic" stdDeviation="4.5" result="blur" />
             <feColorMatrix
               in="blur"
               mode="matrix"
-              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7"
               result="goo"
             />
-            
-            {/* 2. Create heightmap from gooey alpha channel for 3D normals */}
-            <feGaussianBlur in="goo" stdDeviation="2.5" result="heightField" />
-            
-            {/* 3. Specular lighting for realistic metallic highlights */}
-            <feSpecularLighting
-              in="heightField"
-              specularExponent="30"
-              specularConstant="1.6"
-              surfaceScale="5"
-              lighting-color="#ffffff"
-              result="specular"
-            >
-              <feDistantLight azimuth="225" elevation="50" />
-            </feSpecularLighting>
-            
-            {/* 4. Base liquid metal color fill */}
-            <feFlood flood-color="#606668" result="metal-color" />
-            <feComposite in="metal-color" in2="goo" operator="in" result="colored-goo" />
-            
-            {/* 5. Blend specular highlights with base shape */}
-            <feComposite in="specular" in2="goo" operator="in" result="specular-clipped" />
-            <feBlend in="specular-clipped" in2="colored-goo" mode="screen" />
+            {/* 2. Composite the sharp SourceGraphic (with its chrome gradient) on top of the gooey mask */}
+            <feComposite in="SourceGraphic" in2="goo" operator="atop" />
           </filter>
         </defs>
       </svg>
