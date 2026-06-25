37c78f1e9833f8d42f2c41c43faded12fc7aa364
nikhil1234567890123 <nikhilkaundal1247@gmail.com>
2026-06-25 15:29:36 +0530

    style: implement 3D Specular Lighting filter for realistic liquid chrome mercury cursor

--- portfolio_v3/src/components/ui/Cursor.tsx
+++ portfolio_v3/src/components/ui/Cursor.tsx
@@ -25,18 +25,41 @@ const Cursor: React.FC = () => {
 
   return (
     <>
-      {/* Hidden SVG for the gooey liquid / metaball effect threshold filter */}
+      {/* Hidden SVG for the gooey liquid mercury 3D specular chrome filter */}
       <svg xmlns="http://www.w3.org/2000/svg" version="1.1" style={{ display: "none" }}>
         <defs>
           <filter id="gooey-cursor">
+            {/* 1. Gooey blur and alpha threshold to merge shapes into organic blobs */}
             <feGaussianBlur in="SourceGraphic" stdDeviation="6" result="blur" />
             <feColorMatrix
               in="blur"
               mode="matrix"
-              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
               result="goo"
             />
-            <feBlend in="SourceGraphic" in2="goo" />
+            
+            {/* 2. Create heightmap from gooey alpha channel for 3D normals */}
+            <feGaussianBlur in="goo" stdDeviation="2.5" result="heightField" />
+            
+            {/* 3. Specular lighting for realistic metallic highlights */}
+            <feSpecularLighting
+              in="heightField"
+              specularExponent="30"
+              specularConstant="1.6"
+              surfaceScale="5"
+              lighting-color="#ffffff"
+              result="specular"
+            >
+              <feDistantLight azimuth="225" elevation="50" />
+            </feSpecularLighting>
+            
+            {/* 4. Base liquid metal color fill */}
+            <feFlood flood-color="#606668" result="metal-color" />
+            <feComposite in="metal-color" in2="goo" operator="in" result="colored-goo" />
+            
+            {/* 5. Blend specular highlights with base shape */}
+            <feComposite in="specular" in2="goo" operator="in" result="specular-clipped" />
+            <feBlend in="specular-clipped" in2="colored-goo" mode="screen" />
           </filter>
         </defs>
       </svg>
